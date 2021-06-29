19cc95c0d16cdd13e0d3fbac5c0183c544223147
StavBenSimchon <stv.bs101@gmail.com>
2021-06-29 16:00:45 +0300

    .

--- src/org/examples/Brands.groovy
+++ src/org/examples/Brands.groovy
@@ -1,12 +1,5 @@
-// package org.examples
-
-// Class Brands implements Serializable{
-//   def steps
-//   Brands(steps){
-//     this.steps = steps
-//   }
-// }
 package org.examples
+
 class Brands implements Serializable {
   def steps
   Brands(steps) {
