22dd8362c58ed780b2dd6bb6fca4157e4aaeb8dd
StavBenSimchon <stv.bs101@gmail.com>
2021-06-29 15:52:07 +0300

    .

--- src/org/examples/Example.groovy
+++ src/org/examples/Example.groovy
@@ -1,11 +1,5 @@
 package org.examples
-
-Class Example{
-  def var = null
-  Example(){
-    this.var="example"
-  }
-  void myExample(String msg="John") {
-      println msg
-  }
+class Example {
+    Example() {
+    }
 }
