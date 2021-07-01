62edfb23bdf8a16eda61802ee3d03a5def36634d
StavBenSimchon <stv.bs101@gmail.com>
2021-07-01 15:39:23 +0300

    .

--- pipelines/add-whitelist.groovy
+++ pipelines/add-whitelist.groovy
@@ -1,11 +1,11 @@
 @Library("ci@dev") _
-import org.finovation.Tools
+// import org.finovation.Tools
 import org.examples.Brands
 def b = new Brands(this)
-import org.examples.GitClient
-def g = new GitClient(this)
-import org.examples.JiraClient
-def jc = new JiraClient(this)
+// import org.examples.GitClient
+// def g = new GitClient(this)
+// import org.examples.JiraClient
+// def jc = new JiraClient(this)
 
 node {
   stage('init'){
