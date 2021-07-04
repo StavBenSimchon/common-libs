17d12356b1cee4ac2bd86c9a058f9cf54fe95119
StavBenSimchon <stv.bs101@gmail.com>
2021-07-04 17:37:11 +0300

    .

--- src/org/examples/Brands.groovy
+++ src/org/examples/Brands.groovy
@@ -6,7 +6,7 @@ class Brands implements Serializable {
     this.steps=steps
   }
   def parseYaml(){
-    data = steps.readYaml file: "y.yml"
+    def data = steps.readYaml file: "y.yml"
     return data
     // scan_path = data[scan_path]
   }
