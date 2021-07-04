6a4d915dfe7291d10544cb23fc7e0bce3f9269c4
StavBenSimchon <stv.bs101@gmail.com>
2021-07-04 22:37:14 +0300

    .

--- pipelines/add-whitelist.groovy
+++ pipelines/add-whitelist.groovy
@@ -27,9 +27,9 @@ def b = new Brands(this)
       echo "contains"
     } else{
       echo "not contains"
-      boolean fileSuccessfullyDeleted =  new File('z.yml').delete()
+      // boolean fileSuccessfullyDeleted =  new File('z.yml').delete()
       obj['a'].add("7.7.7.7/29")
-      writeYaml file: 'z.yml', data: obj
+      writeYaml file: 'z.yml', data: obj, overwrite: true
       sh '''
         cat ./z.yml
       '''
