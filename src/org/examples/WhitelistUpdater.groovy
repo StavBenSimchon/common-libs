5e024a2b3975739c74e975730cdb7ad860eb6cda
StavBenSimchon <stv.bs101@gmail.com>
2021-07-19 18:21:34 +0300

    .

--- src/org/examples/WhitelistUpdater.groovy
+++ src/org/examples/WhitelistUpdater.groovy
@@ -53,7 +53,7 @@ class WhitelistUpdater implements Serializable {
     // this.steps.println gc
     // this.steps.println gc.declaredMethods.findAll { !it.synthetic }.name
     // def jc = new JiraRestClient()
-    // this.steps.println jc
+    this.steps.println this.steps.env
     Yaml parser = new Yaml()
     Map map = (Map) parser.load(("/var/jenkins_home/jobs/add-whitelist/workspace/y.yml" as File).text);
     return map
