5a61c651ebd7ba1d3ddd2c74b9a95a0701ef6bfe
StavBenSimchon <stv.bs101@gmail.com>
2021-07-28 10:23:08 +0300

    .

--- pipelines/test.groovy
+++ pipelines/test.groovy
@@ -1,9 +1,12 @@
 @Library("ci@dev") _
 import org.examples.JiraClient
-
-def jc = new JiraClient(this, "a")
+j_user = 'automation@finovation.com'
+j_token = 'vHYY25Yx6lyhCe7Fswd11497'
+accessToken = "${j_user}:${j_token}".bytes.encodeBase64().toString()
+def jc = new JiraClient(this, accessToken)
 
 node {
   println jc
-
+  t = "CRM-5213"
+  println jc.getTicketStatus()
 }
